5d7cd41f270caeb3812fd2cea803af5745c80d6c
RoftyMelon <304519498+RoftyMelon@users.noreply.github.com>
2026-09-12 14:47:43 +0200

    Update travel gym wording

--- bloodwork.js
+++ bloodwork.js
@@ -1129,7 +1129,7 @@ window.BLOODWORK =
    "title": "Travel",
    "items": [
     {"text": "Travel after lunch is ideal"},
-    {"text": "Keep your routine during travel - ", "emphasis": "no screens after 9PM and gym in the morning"},
+    {"text": "Keep your routine during travel - ", "emphasis": "no screens after 9PM and gym every morning"},
     {"text": "Family/Friends activities: walking/rucking, juggling, ping pong kit, tennis ball, book reading, board games, language book"}
    ]
   }
